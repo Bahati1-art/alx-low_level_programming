0e68ecfc8c4d4bbf9a7fc2cec83ddcadde78e5dc
Bahati1_art <bahatimabonga1@gmail.com>
2023-06-19 10:34:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
  * main - A programme that prints a line with puts function
  * Return: 0(succes)
  */
+
 int main(void);
+
 {
-puts(" \"Programming is like building a multilingual puzzle");
+
+puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
